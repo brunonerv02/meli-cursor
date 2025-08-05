3c5f27ee0943d00bcf7371b66ef80fb7e095acd7
Bruno <bruno.sgoncalves@mercadolivre.com>
2025-08-04 22:47:15 -0300

    feat: Implement interactive menu with animations and close handlers

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <div className="background-container" />
+      <div className="App">
+        <Header />
+      </div>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './Header.css';
 
-import { imgBg, logoMP, iconShape } from '../../assets/images';
+import { logoMP, iconShape } from '../../assets/images';
+import Portal from '../Portal/Portal';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === 'Escape') {
+        closeMenu();
+      }
+    };
+    if (isMenuOpen) {
+      window.addEventListener('keydown', handleEsc);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [isMenuOpen]);
+
+  const animationVariants = {
+  navbar: {
+    open: {
+      backgroundColor: 'transparent',
+      transition: { duration: 8, ease: 'easeInOut' }
+    },
+    // closed: {
+    //   backgroundColor: '#ffe600',
+    //   transition: { duration: 8, ease: 'easeInOut' }
+    // }
+  },
+  menu: {
+    hidden: {
+      opacity: 0,
+      y: -20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        // when: "beforeChildren",
+        staggerChildren: 0.03
+      }
+    }
+  },
+  menuItem : {
+    hidden: { opacity: 0, y: -10 },
+    visible: { 
+      opacity: 1, 
+      y: 0,
+      transition: {
+        duration: 0.4,
+        ease: "easeOut"
+      }
+    }
+  }
+};
+
+
   return (
     <div
       className="header"
-      style={{ backgroundImage: `url('${imgBg}')` }}
     >
-      <div
+      <motion.div
         className="navbar"
+        variants={animationVariants.navbar}
+        animate={isMenuOpen ? 'open' : 'closed'}
+        initial="closed"
       >
         <div
           className="navbar__container"
@@ -25,6 +93,7 @@ export default function Header() {
             </div>
             <div
               className="navbar__cta-item"
+              onClick={toggleMenu}
             >
               <div
                 className="navbar__cta-text"
@@ -36,15 +105,18 @@ export default function Header() {
               <div
                 className="navbar__cta-icon"
               >
-                <img
+                <motion.img
                   alt="Arrow down"
                   className="navbar__cta-icon-img"
                   src={iconShape}
+                  animate={{ rotate: isMenuOpen ? 180 : 0 }}
+                  transition={{ duration: 0.3 }}
                 />
               </div>
             </div>
             <div
               className="navbar__cta-item"
+              onClick={toggleMenu}
             >
               <div
                 className="navbar__cta-text"
@@ -56,10 +128,12 @@ export default function Header() {
               <div
                 className="navbar__cta-icon"
               >
-                <img
+                 <motion.img
                   alt="Arrow down"
                   className="navbar__cta-icon-img"
                   src={iconShape}
+                  animate={{ rotate: isMenuOpen ? 180 : 0 }}
+                  transition={{ duration: 0.3 }}
                 />
               </div>
             </div>
@@ -100,247 +174,273 @@ export default function Header() {
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className="menu"
-      >
-        <div
-          className="menu__container"
-        >
-          <div
-            className="menu__column"
-          >
-            <div
-              className="menu__image-placeholder"
-            />
-            <div
-              className="menu__title"
-            >
-              <p>
-                A conta completa do Mercado Livre
-              </p>
-            </div>
-          </div>
-          <div
-            className="menu__column"
-          >
-            <div
-              className="menu__category"
-            >
-              <p>
-                BenefÍcios
-              </p>
-            </div>
-            <div
-              className="menu__item"
+      </motion.div>
+      <Portal>
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              layout
+              transition={{ duration: 0.5, ease: [0.00, 0.56, 0.46, 1.00] }}
+              className="menu"
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={animationVariants.menu}
+              onClick={closeMenu}
+              onMouseLeave={closeMenu}
             >
               <div
-                className="menu__item-title"
+                className="menu__container"
+                onClick={(e) => e.stopPropagation()}
               >
-                <p>{`Cofrinhos até 120% do CDI   `}</p>
+                <div
+                  className="menu__column"
+                >
+                  <div
+                    className="menu__image-placeholder"
+                  />
+                  <div
+                    className="menu__title"
+                  >
+                    <p>
+                      A conta completa do Mercado Livre
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="menu__column"
+                >
+                  <motion.div
+                    className="menu__category"
+                    variants={animationVariants.menuItem}
+                  >
+                    <p>
+                      Rendimentos
+                    </p>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>{`Cofrinhos`}</p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Dinheiro separado por objetivos
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>{`Dinheiro na conta`}</p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Rendimento desde o 1º dia
+                      </p>
+                    </div>
+                  </motion.div>
+                </div>
+                <div
+                  className="menu__column"
+                >
+                  <motion.div
+                    className="menu__category"
+                    variants={animationVariants.menuItem}
+                  >
+                    <p>
+                      Crédito
+                    </p>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Cartão de crédito
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Compras em até 18x sem juros
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Linha de crédito
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Rendimento desde o 1º dia
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Empréstimos
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Crédito 100% online e na hora
+                      </p>
+                    </div>
+                  </motion.div>
+                </div>
+                <div
+                  className="menu__column"
+                >
+                  <motion.div
+                    className="menu__category"
+                    variants={animationVariants.menuItem}
+                  >
+                    <p>
+                      Serviços
+                    </p>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Pagamentos
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Pix, boletos e transferências
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Seguros
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Proteção para o que importa
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    className="menu__item"
+                    variants={animationVariants.menuItem}
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Investimentos e Cripto
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>{`Opções a partir de R$ 1 `}</p>
+                    </div>
+                  </motion.div>
+                </div>
+                {/* <div
+                  className="menu__column"
+                >
+                  <div
+                    className="menu__category"
+                  >
+                    <p>
+                      AJUDA
+                    </p>
+                  </div>
+                  <div
+                    className="menu__item"
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Atendimento
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Ajuda e suporte
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    className="menu__item"
+                  >
+                    <div
+                      className="menu__item-title"
+                    >
+                      <p>
+                        Tarifas
+                      </p>
+                    </div>
+                    <div
+                      className="menu__item-subtitle"
+                    >
+                      <p>
+                        Taxas e custos
+                      </p>
+                    </div>
+                  </div>
+                </div> */}
               </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Dinheiro separado por objetivos
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>{`Conta até 105% do CDI `}</p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Rendimento desde o 1º dia
-                </p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="menu__column"
-          >
-            <div
-              className="menu__category"
-            >
-              <p>
-                Crédito
-              </p>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Cartão de crédito
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Compras em até 18x sem juros
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Linha de crédito
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Rendimento desde o 1º dia
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Empréstimos
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Crédito 100% online e na hora
-                </p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="menu__column"
-          >
-            <div
-              className="menu__category"
-            >
-              <p>
-                Serviços
-              </p>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Pagamentos
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Pix, boletos e transferências
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Seguros
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Proteção para o que importa
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Investimentos e Cripto
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>{`Opções a partir de R$ 1 `}</p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="menu__column"
-          >
-            <div
-              className="menu__category"
-            >
-              <p>
-                AJUDA
-              </p>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Atendimento
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Ajuda e suporte
-                </p>
-              </div>
-            </div>
-            <div
-              className="menu__item"
-            >
-              <div
-                className="menu__item-title"
-              >
-                <p>
-                  Tarifas
-                </p>
-              </div>
-              <div
-                className="menu__item-subtitle"
-              >
-                <p>
-                  Taxas e custos
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Portal>
     </div>
   );
 }
